a9fb1c306f5c19cdb8b8f156cd96827634dfc40d
Sabrina Q. Shemet <sqshemet@mit.edu>
2016-01-28 00:31:48 -0500

    I have changes?

--- Mandelbrot.h
+++ Mandelbrot.h
@@ -5,7 +5,7 @@
 class MandelbrotGenerator: public FractalGenerator{
         public:
                 void fillBuffer(uint32_t frame[], int height, int width);
-        private:
+        protected:
                 int max_iterations = 1000;
                 int escapeTime(int row, int col, int height, int width);
                 uint32_t getColor(int escape_time);
